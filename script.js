1484516e4af6d0eb4d03e91d9db90f931fd22382
Asaromi <a.sabililromi@gmail.com>
2023-12-29 14:46:18 +0700

    fix: corner case for invalid font-color

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const button = document.querySelector('input[type="button"]')
 button.addEventListener('click', () => {
   const value = document.querySelector('input[type="number"]').value
 
-  console.log(value);
+  console.log(value)
   if (!value || value === '0') {
     result.innerHTML = 'Invalid value'
+    result.style.color = 'red'
   } else if (value % 3 === 0 && value % 5 === 0) {
     result.innerHTML += 'FizzBuzz!<br>'
   } else if (value % 3 === 0) {
