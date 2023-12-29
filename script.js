f9e30722f78c986805fac2c29c549489f6df8dbf
Asaromi <a.sabililromi@gmail.com>
2023-12-29 14:37:40 +0700

    Finished project with all requirements

---
+++ script.js
@@ -0,0 +1,19 @@
+const result = document.getElementById('result')
+
+const button = document.querySelector('input[type="button"]')
+button.addEventListener('click', () => {
+  const value = document.querySelector('input[type="number"]').value
+
+  console.log(value);
+  if (!value || value === '0') {
+    result.innerHTML = 'Invalid value'
+  } else if (value % 3 === 0 && value % 5 === 0) {
+    result.innerHTML += 'FizzBuzz!<br>'
+  } else if (value % 3 === 0) {
+    result.innerHTML += 'Fizz!<br>'
+  } else if (value % 5 === 0) {
+    result.innerHTML = 'Buzz!<br>'
+  } else {
+    result.innerHTML += value + '<br>'
+  }
+})
